80ea173bc81302df9926303b85d944b6ce32ddcd
abhi Kadam <abhijit.kadam01@hotmail.com>
2025-01-24 23:23:29 +0530

    Intiger

---
+++ Python_Variables/Integer variable.py
@@ -0,0 +1,19 @@
+# Integer variable
+age = 25
+print("Age:", age)
+
+# Float variable
+height = 5.9
+print("Height:", height)
+
+# String variable
+name = "Alice"
+print("Name:", name)
+
+# Boolean variable
+1 == True
+print("Is Student:", 1)
+
+# List variable
+grades = [90, 85, 88]
+print("Grades:", grades)
